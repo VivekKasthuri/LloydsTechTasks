3b1336fead1572ede4c77c0699f4f25ca9cc6fa9
VivekKasthuri <vivekvardhank@gmail.com>
2025-01-07 01:17:24 -0800

    Update LandingViewModel.swift

--- LlyodsTechTest/TechTest/TechTest/LandingScreen/LandingViewModel/LandingViewModel.swift
+++ LlyodsTechTest/TechTest/TechTest/LandingScreen/LandingViewModel/LandingViewModel.swift
@@ -6,25 +6,27 @@
 import Foundation
 import SwiftUI
 
+// MARK:  functions to get the data from Network manager..
 protocol LandingViewRequests {
     
-    func getVerseDetails()async -> Task<BookDetails, Error>
+    func getVerseDetails() async -> Task<BookDetails, Error>
 }
 
-@MainActor class LandingViewModel: ObservableObject, LandingViewRequests {
+// MARK: Viewmodel where actual business logic exists..
+class LandingViewModel: ObservableObject, LandingViewRequests {
     
     @Published var details: BookDetails?
     private var manager: ApiManagerRequests?
-    
     init(manager: ApiManagerRequests = ApiManager()) {
         self.manager = manager
     }
     
-    func getVerseDetails()async -> Task<BookDetails, Error> {
+    // Get the values from api call...
+    func getVerseDetails() async -> Task<BookDetails, Error> {
+        
         return Task {
-            guard let details: BookDetails = try await manager?.getData(endPoint: ApiEndPoint.baseUrl.rawValue + ApiEndPoint.typeOfVerse.rawValue) else {return self.details!}
+            guard let details: BookDetails = try await manager?.getData(endPoint: ApiEndPoint.baseUrl.rawValue + ApiEndPoint.typeOfVerse.rawValue) else { return self.details! }
             return details
         }
     }
-
 }
