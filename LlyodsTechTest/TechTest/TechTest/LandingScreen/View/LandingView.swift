b14e244ab5f2457d9ef637b15e7313a3bd1e6f42
VivekKasthuri <vivekvardhank@gmail.com>
2025-01-07 01:15:06 -0800

    Update LandingView.swift

--- LlyodsTechTest/TechTest/TechTest/LandingScreen/View/LandingView.swift
+++ LlyodsTechTest/TechTest/TechTest/LandingScreen/View/LandingView.swift
@@ -8,32 +8,23 @@
 import SwiftUI
 
 struct LandingView: View {
-   @StateObject var viewModel = LandingViewModel()
-   @State var isLoaded: Bool = false
+    @StateObject var viewModel = LandingViewModel()
+    @State var isLoaded: Bool = false
     var body: some View {
         ZStack{
             VStack(alignment: .leading, spacing: 5.0) {
-                ReusableText(textTitle: viewModel.details?.verse ?? "", font: .title)
-                if isLoaded {Divider()}
+                ReusableText(textTitle: viewModel.details?.verse ?? "", font: .title) // Reusable component...
+                if isLoaded { Divider() }
                 ReusableText(textTitle: viewModel.details?.translation ?? "", font:.title3, color: .blue)
                 Spacer()
             }
         }
-
         .task {
-            let task = await viewModel.getVerseDetails()
+            let task = await viewModel.getVerseDetails() // Network call..
             if let verses = try? await task.value {
                 viewModel.details = verses
                 isLoaded = true
-          }
+            }
         }
-    }
-  }
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        LandingView()
     }
 }
